ab3033b70627fd6f16933ed07f42553fda166347
Carlos Andres Mendoza Martinez <carlos.mendoza@ghenova.com>
2023-05-31 15:09:56 -0500

    Segundo intento

--- lion_takanawa/cloud.js
+++ lion_takanawa/cloud.js
@@ -1,28 +1,67 @@
 {
-	"version": "1.7",
-	"octreeDir": "data",
-	"boundingBox": {
-		"lx": -0.748212993144989,
-		"ly": -2.78040599822998,
-		"lz": 2.54782128334045,
-		"ux": 3.89967638254166,
-		"uy": 1.86748337745667,
-		"uz": 7.1957106590271
-	},
-	"tightBoundingBox": {
-		"lx": -0.748212993144989,
-		"ly": -2.78040599822998,
-		"lz": 2.55100011825562,
-		"ux": 2.4497377872467,
-		"uy": 1.48934376239777,
-		"uz": 7.1957106590271
-	},
-	"pointAttributes": [
-		"POSITION_CARTESIAN",
-		"COLOR_PACKED",
-		"NORMAL_SPHEREMAPPED"
-	],
-	"spacing": 0.0750000029802322,
-	"scale": 0.001,
-	"hierarchyStepSize": 6
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 867672,
+    "boundingBox": {
+        "lx": 260.37054443359377,
+        "ly": -57.481414794921878,
+        "lz": 322.3431091308594,
+        "ux": 379.13983154296877,
+        "uy": 61.287872314453128,
+        "uz": 441.1123962402344
+    },
+    "tightBoundingBox": {
+        "lx": 260.370544,
+        "ly": -57.481415,
+        "lz": 322.34310899999999,
+        "ux": 308.60965,
+        "uy": 61.287872,
+        "uz": 415.660645
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.0285722017288209,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
 }
